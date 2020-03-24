d23a248dc4e7a0cad3326c1dcf119a97268089b0
Ian Effendi <effendiian@gmail.com>
2020-03-24 17:29:02 -0400

    Add the Dog model

---
+++ server/models/Dog.js
@@ -0,0 +1,51 @@
+// Import mongoose.
+const mongoose = require('mongoose');
+
+// Create the model.
+let DogModel = {};
+
+// Create the schema.
+const DogSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    breed: {
+        type: String,
+        require: true,
+        trim: true,
+        unique: false
+    },
+
+    age: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+// Static functions.
+DogSchema.statics.findByName = (name, callback) => {
+    const search = {
+        name,
+    };
+
+    return DogModel.findOne(search, callback);
+};
+
+// Create the model.
+DogModel = mongoose.model('Dog', DogSchema);
+
+// Export.
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
